00691d5cb6f87cfe8995711381e08ed24c580892
thanhdinh21498-lab <thanh.dinh21498@gmail.com>
2025-12-09 13:17:14 -0500

    Update app.py

--- Global Dexterity/app.py
+++ Global Dexterity/app.py
@@ -40,7 +40,7 @@ with st.sidebar:
 
 # ---------- TITLE ----------
 with st.expander("For folks having situation about small talk:"):
-    st.subhearder("Try to step out")
+    st.write("Try to step out and talk to high profile people but make sure you know what to talk about")
     Tatum_path = IMG_DIR / "Tatum.jpg"
     if Tatum_path.exists():
         st.image(
@@ -248,6 +248,7 @@ in a clearer way while keeping respect and relationships in mind.
 )
 
 st.success("Thank you for viewing my Global Dexterity project!")
+
 
 
 
